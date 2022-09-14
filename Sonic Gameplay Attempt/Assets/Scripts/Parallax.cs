9ff1186f9d6d3b7263cb7e446573bf4d1ab7c2f8
Nitrohedge21 <ersanefe21@gmail.com>
2022-09-14 17:33:45 +0100

    Unity Version Change
    -Tried adding a parallax using a tutorial but it's kinda glitchy. Gonna have to fix that.
    - The background also goes over the hud for some reason.( LITERALLY FOUND THE ISSUE AS I WAS TYPING THIS).

--- Sonic Gameplay Attempt/Assets/Scripts/Parallax.cs
+++ Sonic Gameplay Attempt/Assets/Scripts/Parallax.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class Parallax : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Material mat;
+    float distance;
+
+    [Range(0f, 0.5f)]
+    public float speed = 0.2f;
     void Start()
     {
-        
+        mat = GetComponent<Renderer>().material;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        distance += Time.deltaTime * speed;
+        mat.SetTextureOffset("_MainTex", Vector2.right * distance);
     }
 }
